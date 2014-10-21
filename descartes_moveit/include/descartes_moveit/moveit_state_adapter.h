6c3adf7c202139a789b2d08e582ae77b1d6cafd6
Shaun Edwards <shaun.edwards@gmail.com>
2014-10-20 22:39:06 -0400

    Added Moveit adapter package

---
+++ descartes_moveit/include/descartes_moveit/moveit_state_adapter.h
@@ -0,0 +1,110 @@
+/*
+ * Software License Agreement (Apache License)
+ *
+ * Copyright (c) 2014, Dan Solomon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOVEIT_STATE_ADPATER_H_
+#define MOVEIT_STATE_ADPATER_H_
+
+#include "descartes_trajectory_planning/robot_model.h"
+#include "moveit/robot_model/robot_model.h"
+#include "moveit/kinematics_base/kinematics_base.h"
+#include <string>
+
+namespace descartes_moveit
+{
+
+/**@brief MoveitStateAdapter adapts the MoveIt RobotState to the Descartes RobotModel interface
+ *
+ */
+class MoveitStateAdapter : public descartes::RobotModel
+{
+public:
+
+  /**
+   * Constructor for Moveit state adapters (implements Descartes robot model interface)
+   * @param robot_state robot state object utilized for kinematic/dynamic state checking
+   * @param group_name planning group name
+   * @param tool_frame tool frame name
+   * @param wobj_frame work object frame name
+   */
+  MoveitStateAdapter(const moveit::core::RobotState & robot_state, const std::string & group_name,
+                    const std::string & tool_frame, const std::string & wobj_frame);
+  virtual ~MoveitStateAdapter()
+  {
+  }
+  ;
+
+  virtual bool getIK(const Eigen::Affine3d &pose, const std::vector<double> &seed_state,
+                     std::vector<double> &joint_pose) const;
+
+  virtual bool getAllIK(const Eigen::Affine3d &pose, std::vector<std::vector<double> > &joint_poses) const;
+
+  virtual bool getFK(const std::vector<double> &joint_pose, Eigen::Affine3d &pose) const;
+
+  virtual bool isValid(const std::vector<double> &joint_pose) const;
+
+  virtual bool isValid(const Eigen::Affine3d &pose) const;
+
+protected:
+
+  /**
+   * @brief Default constructor hidden
+   */
+  MoveitStateAdapter()
+  {
+  }
+  ;
+
+  /**
+   * Gets IK solution (assumes robot state is pre-seeded)
+   * @param pose Affine pose of TOOL in WOBJ frame
+   * @param joint_pose Solution (if function successful).
+   * @return
+   */
+  bool getIK(const Eigen::Affine3d &pose, std::vector<double> &joint_pose) const;
+
+  /**
+   * @brief Pointer to moveit robot state (mutable object state is reset with
+   * each function call
+   */
+  mutable moveit::core::RobotStatePtr robot_state_;
+
+  /**
+   * @brief Planning group name
+   */
+  std::string group_name_;
+
+  /**
+   * @brief Tool frame name
+   */
+  std::string tool_base_;
+
+  /**
+   * @brief Work object/reference frame name
+   */
+  std::string wobj_base_;
+
+  /**
+   * @brief Joint solution sample iterations for returning "all" joints
+   */
+  size_t sample_iterations_;
+
+};
+
+} //descartes_moveit
+
+#endif /* MOVEIT_STATE_ADPATER_H_ */
---
+++ descartes_moveit/src/moveit_state_adapter.cpp
@@ -0,0 +1,180 @@
+/*
+ * Software License Agreement (Apache License)
+ *
+ * Copyright (c) 2014, Dan Solomon
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <console_bridge/console.h>
+#include "descartes_moveit/moveit_state_adapter.h"
+#include "eigen_conversions/eigen_msg.h"
+#include "random_numbers/random_numbers.h"
+#include "descartes_trajectory_planning/pretty_print.hpp"
+#include <sstream>
+
+#define NOT_IMPLEMENTED_ERR logError("%s not implemented", __PRETTY_FUNCTION__)
+
+namespace descartes_moveit
+{
+
+MoveitStateAdapter::MoveitStateAdapter(const moveit::core::RobotState & robot_state, const std::string & group_name,
+                                     const std::string & tool_frame, const std::string & wobj_frame) :
+    robot_state_(new moveit::core::RobotState(robot_state)), group_name_(group_name),
+    tool_base_(tool_frame), wobj_base_(wobj_frame)
+{
+
+  moveit::core::RobotModelConstPtr robot_model_ = robot_state_->getRobotModel();
+
+  if (robot_model_->hasJointModel(group_name_))
+  {
+    std::vector<std::string> joint_names = robot_model_->getJointModelGroup(group_name_)->getJointModelNames();
+    if (tool_base_ != joint_names.back())
+    {
+      logError("Tool: %s does not match group tool: %s, functionality will be implemented in the future",
+               tool_base_.c_str(), joint_names.front().c_str());
+    }
+    if (wobj_base_ != joint_names.back())
+    {
+      logError("Work object: %s does not match group base: %s, functionality will be implemented in the future",
+               wobj_base_.c_str(), joint_names.back().c_str());
+    }
+  }
+  else
+  {
+    logError("Joint group: %s does not exist in robot model");
+  }
+  return;
+}
+
+bool MoveitStateAdapter::getIK(const Eigen::Affine3d &pose, const std::vector<double> &seed_state,
+                              std::vector<double> &joint_pose) const
+{
+  robot_state_->setJointGroupPositions(group_name_, seed_state);
+  return getIK(pose, joint_pose);
+}
+
+bool MoveitStateAdapter::getIK(const Eigen::Affine3d &pose, std::vector<double> &joint_pose) const
+{
+  bool rtn = false;
+  if (robot_state_->setFromIK(robot_state_->getJointModelGroup(group_name_), pose, tool_base_))
+  {
+    joint_pose.resize(robot_state_->getVariableCount());
+    for (size_t ii = 0; ii < robot_state_->getVariableCount(); ++ii)
+    {
+      joint_pose[ii] = robot_state_->getVariablePosition(ii);
+    }
+    rtn = true;
+  }
+  else
+  {
+    logError("Could not set Cartesian pose.");
+    rtn = false;
+  }
+  return rtn;
+}
+
+bool MoveitStateAdapter::getAllIK(const Eigen::Affine3d &pose, std::vector<std::vector<double> > &joint_poses) const
+{
+  joint_poses.clear();
+  for (size_t sample_iter = 0; sample_iter < sample_iterations_; ++sample_iter)
+  {
+    robot_state_->setToRandomPositions();
+    std::vector<double> joint_pose;
+    if (getIK(pose, joint_pose))
+    {
+      std::vector<std::vector<double> >::iterator it;
+
+      it = find(joint_poses.begin(), joint_poses.end(), joint_pose);
+      if (it == joint_poses.end())
+      {
+        std::stringstream msg;
+        msg << "Found *new* solution on " << sample_iter << " iteration, joint: " << *it;
+        logDebug(msg.str().c_str());
+        joint_poses.push_back(joint_pose);
+      }
+    }
+  }
+  logDebug("Found %d joint solutions out of %d iterations", joint_poses.size(), sample_iterations_);
+  if (joint_poses.empty())
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+bool MoveitStateAdapter::getFK(const std::vector<double> &joint_pose, Eigen::Affine3d &pose) const
+{
+  bool rtn = false;
+  robot_state_->setVariablePositions(joint_pose);
+  if ( isValid(joint_pose) )
+  {
+  if (robot_state_->knowsFrameTransform(tool_base_))
+  {
+    pose = robot_state_->getFrameTransform(tool_base_);
+    rtn = true;
+  }
+  else
+  {
+    logError("Robot state does not recognize tool frame: %s", tool_base_.c_str());
+    rtn = false;
+  }
+  }
+  else
+  {
+    logError("Invalid joint pose passed to get forward kinematics");
+    rtn = false;
+  }
+  return rtn;
+}
+
+bool MoveitStateAdapter::isValid(const std::vector<double> &joint_pose) const
+{
+  bool rtn = false;
+  if (robot_state_->getVariableCount() == joint_pose.size())
+  {
+    robot_state_->setVariablePositions(joint_pose);
+    robot_state_->setVariableVelocities(std::vector<double>(joint_pose.size(), 0.));
+    robot_state_->setVariableAccelerations(std::vector<double>(joint_pose.size(), 0.));
+    if (robot_state_->satisfiesBounds())
+    {
+      rtn = true;
+    }
+    else
+    {
+      std::stringstream msg;
+      msg << "Joint pose: " << joint_pose << ", outside joint boundaries";
+      logDebug(msg.str().c_str());
+    }
+  }
+  else
+  {
+    logError("Size of joint pose: %d doesn't match robot state variable size: %d",
+             joint_pose.size(), robot_state_->getVariableCount());
+    rtn = false;
+  }
+  return rtn;
+}
+
+bool MoveitStateAdapter::isValid(const Eigen::Affine3d &pose) const
+{
+  //TODO: Could check robot extents first as a quick check
+  std::vector<double> dummy;
+  return getIK(pose, dummy);
+}
+
+} //descartes_moveit
+
