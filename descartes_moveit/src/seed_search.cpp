f64ae69d2afdee68bd6538505d0197751e192b3c
Jonathan Meyer <jonathan.meyer@swri.org>
2015-04-09 14:30:30 -0500

    Code re-arrangements and clean up. Simplified for case of joint pairs

--- descartes_moveit/src/seed_search.cpp
+++ descartes_moveit/src/seed_search.cpp
@@ -1,32 +1,3 @@
-﻿/*
- * Software License Agreement (Apache License)
- *
- * Copyright (c) 2015, Southwest Research Institute
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *
- * This file defines methods that can be used to generate seed values for iterative
- * numerical solvers used by Moveit for a generic robot.
- *
- * Users can call the seed::findSeedStates() function with a robot state object, a
- * particular Moveit move group, and a series of joint pairs. These joint pairs
- * should define arm configurations such as elbow up or elbow down. For many 6 DOF
- * robots, joints 2 & 3 and joints 4 & 6 (starting counting at 1) will form elbow
- * and wrist configurations.
- *
- */
-
 #include <descartes_moveit/seed_search.h>
 
 #include <ros/ros.h>
@@ -34,131 +5,11 @@
 using namespace descartes_moveit;
 
 typedef std::vector<unsigned> BijectionVec;
+typedef std::pair<unsigned, unsigned> JointPair;
+typedef std::vector<JointPair> JointPairVec;
+typedef std::vector<double> JointConfig;
+typedef std::vector<JointConfig> JointConfigVec;
 
-/**
- * @brief A ComboHandle is a (relatively) light weight and precise way
- *        to represent a joint rotation permutation. Can work with up to
- *        255 joint discretizations and a lot of joints.
- */
-struct ComboHandle
-{
-  ComboHandle(size_t n)
-    : combos_(n)
-  {}
-
-  uint8_t& operator[](size_t idx)
-  {
-    return combos_[idx];
-  }
-
-  const uint8_t& operator[](size_t idx) const
-  {
-    return combos_[idx];
-  }
-
-  const std::size_t size() const { return combos_.size(); }
-
-private:
-  std::vector<uint8_t> combos_;
-};
-
-/**
- * @brief The CombinationManager class is responsible for creating
- *        joint permutations given the number of joints and the
- *        number of discretizations per joint.
- */
-class CombinationManager
-{
-public:
-  CombinationManager(unsigned n_joints, unsigned n_discs)
-    : n_joints_(n_joints)
-    , n_discs_(n_discs)
-  {}
-
-  size_t count() const
-  {
-    size_t result = 1;
-    for (size_t i = 0; i < n_joints_; ++i) result *= n_discs_;
-    return result;
-  }
-
-  ComboHandle fromNumber(size_t n)
-  {
-    ComboHandle result (n_joints_);
-    for (size_t i = 0; i < n_joints_; i++)
-    {
-      result[n_joints_-1-i] = static_cast<uint8_t>(n % n_discs_);
-      n /= n_discs_;
-    }
-    return result;
-  }
-
-private:
-  unsigned n_joints_;
-  unsigned n_discs_;
-};
-
-/**
- * @brief Comparison operator so we can use ComboHandles in std::set
- */
-bool operator<(const ComboHandle& lhs, const ComboHandle& rhs)
-{
-  for (std::size_t i = 0; i < lhs.size(); ++i)
-  {
-    if ( lhs[i] != rhs[i] )
-    {
-      return lhs[i] < rhs[i];
-    }
-  }
-  // equal
-  return false;
-}
-
-// Simple container for holding info necessary for converting from handles to joint configs
-class ConfigCreator
-{
-public:
-  /**
-   * @brief ConfigCreator constructor
-   * @param initial_joint_pos The values for all joints that are not being permuted
-   * @param bij The bijection vector where the first values in the bijection are the
-   *            joints being permuted
-   * @param min_angle The lower limit of the joint angles
-   * @param step_angle Joint angle discretization
-   */
-  ConfigCreator(const std::vector<double> initial_joint_pos,
-                const BijectionVec& bij, double min_angle, double step_angle)
-    : initial_joint_positions_(initial_joint_pos)
-    , bij_(bij)
-    , min_angle_(min_angle)
-    , step_angle_(step_angle)
-  {}
-
-  seed::JointConfig toJointConfig(const ComboHandle& handle) const
-  {
-    seed::JointConfig result;
-    result.resize(bij_.size());
-
-    std::size_t i;
-    for (i = 0; i < handle.size(); i++)
-    {
-      result[bij_[i]] = min_angle_ + handle[i] * step_angle_;
-    }
-
-    for (; i < bij_.size(); ++i)
-    {
-      result[bij_[i]] = initial_joint_positions_[bij_[i]];
-    }
-
-    return result;
-  }
-
-private:
-  std::vector<double> initial_joint_positions_;
-  BijectionVec bij_;
-  double min_angle_;
-  double step_angle_;
-};
 
 /**
  * @brief Forward kinematics helper
@@ -166,7 +17,7 @@ private:
 bool doFK(moveit::core::RobotState& state,
           const moveit::core::JointModelGroup* group,
           const std::string& tool,
-          const seed::JointConfig& joint_pose,
+          const JointConfig& joint_pose,
           Eigen::Affine3d& result)
 {
   state.setJointGroupPositions(group, joint_pose);
@@ -195,11 +46,14 @@ bool doIK(moveit::core::RobotState& state,
           const std::string& group_name,
           const std::string& tool,
           const Eigen::Affine3d& pose,
-          const seed::JointConfig& seed,
-          seed::JointConfig& result)
+          const JointConfig& seed,
+          JointConfig& result)
 {
+  const static int N_ATTEMPTS = 1;
+  const static double IK_TIMEOUT = 0.01;
+
   state.setJointGroupPositions(group_name, seed);
-  if (!state.setFromIK(group, pose, tool, 1, 0.01))
+  if (!state.setFromIK(group, pose, tool, N_ATTEMPTS, IK_TIMEOUT))
   {
     return false;
   }
@@ -211,8 +65,8 @@ bool doIK(moveit::core::RobotState& state,
  * @brief Returns true if the determinant of the jacobian is not zero.
  *        Returns false if it is close to zero (and robot is near singularity)
  */
-bool checkSingularity(moveit::core::RobotState& state,
-                      const moveit::core::JointModelGroup* group)
+bool isSingularity(moveit::core::RobotState& state,
+                   const moveit::core::JointModelGroup* group)
 {
  Eigen::Vector3d reference_point_position(0.0, 0.0, 0.0);
  Eigen::MatrixXd jacobian;
@@ -224,162 +78,150 @@ bool checkSingularity(moveit::core::RobotState& state,
 
 // Compares the first n_compare bijection joint values from a and b and tests
 // whether they are within 45 degrees of one another
-inline bool sameJointConfig(const seed::JointConfig& a, const seed::JointConfig& b,
-                            const BijectionVec& bij, unsigned n_compare)
+inline bool isSameJointConfig(const JointConfig& a, const JointConfig& b, const JointPair& pair)
 {
-  for (std::size_t i = 0; i < n_compare; i++)
-  {
-    if (std::fmod(std::abs(a[bij[i]] - b[bij[i]]), 2*M_PI) > M_PI_4) return false;
-  }
-  return true;
+  const static double MIN_DIFF = M_PI_4;
+  return std::abs(a[pair.first] - b[pair.first]) < MIN_DIFF &&
+         std::abs(a[pair.second] - b[pair.second]) < MIN_DIFF;
 }
 
 // Tests whether a given joint configuration, c, is similar to any joint set already
 // in set using the given bijection values and number of joints.
-inline bool inJointSet(const seed::JointConfig& c, const seed::JointConfigVec& set,
-                       const BijectionVec& bij, unsigned n_compare)
+inline bool isInJointSet(const JointConfig& c, const JointConfigVec& set, const JointPair& pair)
 {
   for (std::size_t i = 0; i < set.size(); ++i)
   {
-    if (sameJointConfig(c, set[i], bij, n_compare)) return true;
+    if (isSameJointConfig(c, set[i], pair)) return true;
   }
   return false;
 }
 
-// Creates a bijection vector which puts the permuted pair of joints
-// up front.
-BijectionVec createBijection(const seed::JointPair& pair, unsigned dof)
+std::vector<double> createValidJointPositions(const moveit::core::JointModel& model,
+                                              double increment)
 {
-  BijectionVec bij;
-  bij.reserve(dof);
+  const moveit::core::JointModel::Bounds& bounds = model.getVariableBounds();
+  double min = bounds[0].min_position_;
+  double max = bounds[0].max_position_;
 
-  // add first two joints
-  bij.push_back(pair.first);
-  bij.push_back(pair.second);
-
-  for (std::size_t i = 0; i < dof; i++)
+  std::vector<double> result;
+  while (min <= max)
   {
-    if (i != pair.first && i != pair.second)
-      bij.push_back(i);
+      result.push_back(min);
+      min += increment;
   }
 
-  return bij;
+  return result;
 }
 
-// Uses ROS's random number generator to fill out a vector of doubles (length dof)
-// with uniform values between min and max
-std::vector<double> randomInitialState(unsigned dof, double min, double max)
+std::vector<double> createSeedFromPerms(const std::vector<double>& initial,
+                                        const std::vector<double>& a_perms,
+                                        unsigned a_joint_idx,
+                                        const std::vector<double>& b_perms,
+                                        unsigned b_joint_idx,
+                                        unsigned n)
 {
-  std::vector<double> result;
-  result.reserve(dof);
-
-  random_numbers::RandomNumberGenerator gen;
-
-  for (std::size_t i = 0; i < dof; ++i)
-  {
-    result.push_back(gen.uniformReal(min, max));
-  }
-  return result;
+  std::vector<double> seed = initial;
+  double a_value = a_perms[n / b_perms.size()];
+  double b_value = b_perms[n % b_perms.size()];
+  seed[a_joint_idx] = a_value;
+  seed[b_joint_idx] = b_value;
+  return seed;
 }
 
 /**
  * @brief Returns possible seed states generated from permuting the given pair of joints
  *        (using joint numbering starting at 0)
  */
-seed::JointConfigVec findSeedStatesForPair(moveit::core::RobotStatePtr state,
-                                           const std::string& group_name,
-                                           const std::string& tool_frame,
-                                           const seed::JointPair& pair)
+JointConfigVec findSeedStatesForPair(moveit::core::RobotStatePtr state,
+                                     const std::string& group_name,
+                                     const std::string& tool_frame,
+                                     const JointPair& pair)
 {
-  const moveit::core::JointModelGroup* group = state->getJointModelGroup(group_name);
+  using namespace moveit::core;
+  
+  const JointModelGroup* group = state->getJointModelGroup(group_name);
+  const std::vector<const JointModel*> active_joints = group->getActiveJointModels();
 
-  // Hard-coded constants for joint iteration
-  // Should be replaced by interface that uses moveit's joint tolerances
-  // on a per joint basis
-  const double min_angle = -M_PI;
-  const double step_angle = M_PI_2;
-  const unsigned steps = 4;
-  const unsigned n_permuted_joints = 2;
-
-  const unsigned dof = group->getActiveJointModelNames().size();
-
-  // Create bijection and initial state (for non-permuted joints)
-  BijectionVec bij = createBijection(pair, dof);
-  std::vector<double> init_state = randomInitialState(dof, -M_PI_2, M_PI_2);
-
-  // Create classes that will create permutations and
-  // the resulting joint configurations
-  CombinationManager combos (n_permuted_joints, steps);
-  ConfigCreator configs (init_state, bij, min_angle, step_angle);
-
-  // A set of joint permutation handles that lead to unique IK solutions for one
-  // of the tested poses. This set, when transformed to joint configurations, is
-  // the result of this function
-  std::set<ComboHandle> global_seed_states;
-
-  // Outer loop -> for every permutation calculate pose
-  for (std::size_t i = 0; i < combos.count(); ++i)
+  // check each joint to see if it is revolute
+  for (const JointModel* model : active_joints)
   {
-    auto round_handle = combos.fromNumber(i);
-    auto round_config = configs.toJointConfig(round_handle);
+      if (model->getType() != JointModel::REVOLUTE)
+          ROS_WARN_STREAM("Joint '" << model->getName() << "' does not appear to be revolute");
+  }
 
-    // Data structures for this round alone
-    std::vector<ComboHandle> this_round_seeds;
-    seed::JointConfigVec this_round_iks;
+  // compute random starting values for all joints
+  state->setToRandomPositions();
+  JointConfig init_state;
+  state->copyJointGroupPositions(group, init_state);
+
+  // Precompute the valid positions for the two joints we'll iterate over
+  std::vector<double> joint1_perms = createValidJointPositions(*active_joints[pair.first], M_PI_2);
+  std::vector<double> joint2_perms = createValidJointPositions(*active_joints[pair.second], M_PI_2);
+
+  std::set<size_t> final_seed_states;
+
+  // Walk the valid combos
+  const size_t iterations = joint1_perms.size() * joint2_perms.size();
+  for (std::size_t i = 0; i < iterations; ++i)
+  {
+    // Make the seed state for this index from permutations
+    JointConfig round_ik = createSeedFromPerms(init_state, joint1_perms, pair.first,
+                                               joint2_perms, pair.second, i);
+    // Containers for the valid seeds and ik solutions for THIS round
+    std::vector<size_t> this_round_seeds;
+    JointConfigVec this_round_iks;
 
     // Forward kinematics
     Eigen::Affine3d target_pose;
-    if (!doFK(*state, group, tool_frame, round_config, target_pose))
+    if (!doFK(*state, group, tool_frame, round_ik, target_pose))
     {
       ROS_DEBUG_STREAM("No FK for pose " << i);
       continue;
     }
 
     // Check to make sure we're not in a singularity
-    if (!checkSingularity(*state, group))
+    if (!isSingularity(*state, group))
     {
       ROS_DEBUG_STREAM("Pose " << i << " at singularity.");
       continue;
     }
 
     // Add initial states to the iks/seeds for this point
-    this_round_seeds.push_back(round_handle);
-    this_round_iks.push_back(round_config);
+    this_round_seeds.push_back(i);
+    this_round_iks.push_back(round_ik);
 
-    // Now try the seeds that have worked so far. The hope is to create
-    // a minimal set of seeds that can solve for a given set of poses.
-    for (const auto& s : global_seed_states)
+    // Now we'll walk through all of the other seeds, starting with the ones that have worked so far
+    for (size_t idx : final_seed_states)
     {
       // create joint config fom handle
-      seed::JointConfig seed = configs.toJointConfig(s);
-      seed::JointConfig ik;
+      JointConfig seed = createSeedFromPerms(init_state, joint1_perms, pair.first,
+                                             joint2_perms, pair.second, idx);
+      JointConfig ik;
       // perform ik
       if (!doIK(*state, group, group_name, tool_frame, target_pose, seed, ik))
       {
         continue;
       }
       // If we have a unique IK solution, then add to the iks seen this round
-      if (!inJointSet(ik, this_round_iks, bij, n_permuted_joints))
+      if (!isInJointSet(ik, this_round_iks, pair))
       {
         this_round_iks.push_back(ik);
-        this_round_seeds.push_back(s);
+        this_round_seeds.push_back(idx);
       }
     }
 
-    // Now try the rest of the seeds
-    for (std::size_t j = 0; j < combos.count(); j++)
+    // Now try the rest of the joint possible seed states if they haven't been tried
+    for (std::size_t j = 0; j < iterations; ++j)
     {
-      // skip the situation where pose is generated from current seed
-      // we already have that ik
+      // skip the situation where pose is generated from current seed we already have that ik
       if (i == j) continue;
 
-      auto handle = combos.fromNumber(j);
-      // is the handle in the global solution set yet?
-      auto count = global_seed_states.count(handle);
+      unsigned count = final_seed_states.count(j);
       if (count != 0) continue;
 
-      seed::JointConfig seed = configs.toJointConfig(handle);
-      seed::JointConfig ik;
+      JointConfig seed = createSeedFromPerms(init_state, joint1_perms, pair.first,
+                                             joint2_perms, pair.second, j);
+      JointConfig ik;
       if (!doIK(*state, group, group_name, tool_frame, target_pose, seed, ik))
       {
         continue;
@@ -388,67 +230,58 @@ seed::JointConfigVec findSeedStatesForPair(moveit::core::RobotStatePtr state,
       // If we have a new IK solution here, then it was generated
       // by a seed that has not yet been added to the overall seed
       // states to be returned
-      if (!inJointSet(ik, this_round_iks, bij, n_permuted_joints))
+      if (!isInJointSet(ik, this_round_iks, pair))
       {
         this_round_iks.push_back(ik);
-        this_round_seeds.push_back(handle);
-        global_seed_states.insert(handle);
+        this_round_seeds.push_back(j);
+        final_seed_states.insert(j);
       }
     }
 
     ROS_DEBUG_STREAM("Calculated " << this_round_iks.size() << " unique IK states this round");
   } // outer loop end
 
-  // Consolidate into answer and return
-  seed::JointConfigVec result;
-  result.reserve(global_seed_states.size());
-  for (auto it = global_seed_states.begin(); it != global_seed_states.end(); ++it)
+  // Consolidate set into vector for the result
+  JointConfigVec result;
+  result.reserve(final_seed_states.size());
+  for (size_t idx : final_seed_states)
   {
-    result.push_back(configs.toJointConfig(*it));
+    result.push_back(createSeedFromPerms(init_state, joint1_perms,
+                                         pair.first, joint2_perms,
+                                         pair.second, idx));
   }
   return result;
 }
 
-/**
- * @brief Returns a sequence of seed states for iterative inverse kinematic solvers to use
- *        when 'sampling' the solution space of a pose. These seeds are generated by
- *        iterating through all possible joint permutations of each pair of joints passed
- *        in by the user.
- * @param state Shared pointer to robot state used to perform FK/IK
- * @param group_name Name of the move group for which to generate seeds
- * @param tool_frame The name of the tool link in which to work with FK/IK
- * @param pairs A sequence of joint pairs used to generate the seed states.
- * @return A vector of seed states
- */
-seed::JointConfigVec seed::findSeedStates(moveit::core::RobotStatePtr state,
-                                          const std::string& group_name,
-                                          const std::string& tool_frame,
-                                          const JointPairVec& pairs)
+JointConfigVec seed::findSeedStates(moveit::core::RobotStatePtr state,
+                                    const std::string& group_name,
+                                    const std::string& tool_frame,
+                                    const JointPairVec& pairs)
 {
-  seed::JointConfigVec result;
+  JointConfigVec result;
   for (const auto& pair : pairs)
   {
-    seed::JointConfigVec partial_answer = findSeedStatesForPair(state, group_name, tool_frame, pair);
+    JointConfigVec partial_answer = findSeedStatesForPair(state, group_name, tool_frame, pair);
     result.insert(result.end(), partial_answer.begin(), partial_answer.end());
   }
   return result;
 }
 
-seed::JointConfigVec seed::findRandomSeeds(moveit::core::RobotStatePtr state,
-                                           const std::string& group_name,
-                                           unsigned n)
+JointConfigVec seed::findRandomSeeds(moveit::core::RobotStatePtr state,
+                                     const std::string& group_name,
+                                     unsigned n)
 {
   auto group = state->getJointModelGroup(group_name);
 
   JointConfigVec result;
-  const unsigned max_iterations = n * 2;
-
-  for (unsigned i = 0; i < max_iterations && result.size() < n; ++i)
+  
+  for (unsigned i = 0; i < n; ++i)
   {
-    JointConfig c;
-
     state->setToRandomPositions();
+
+    JointConfig c;
     state->copyJointGroupPositions(group, c);
+    
     result.push_back(c);
   }
   return result;
