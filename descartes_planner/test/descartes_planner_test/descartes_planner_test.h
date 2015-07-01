e92b6127f6bc89dcda2fcbd7bd3d8d48099e3039
madratman <ratneshmadaan@gmail.com>
2015-06-30 22:54:26 -0400

    allows for different size inputs to sparse and dense planner tests in a single test run

--- descartes_planner/test/descartes_planner_test/descartes_planner_test.h
+++ descartes_planner/test/descartes_planner_test/descartes_planner_test.h
@@ -24,6 +24,9 @@
 #include "descartes_core/pretty_print.hpp"
 #include "descartes_core/robot_model.h"
 #include "descartes_core/path_planner_base.h"
+#include "descartes_planner/sparse_planner.h"
+#include "descartes_planner/dense_planner.h"
+#include "descartes_planner/planning_graph.h"
 #include "ThreeDOFRobot.h"
 #include "TestPoint.h"
 #include "ros/console.h"
@@ -66,15 +69,28 @@ public:
 };
 
 using namespace descartes_core;
+using namespace descartes_planner;
 using namespace descartes_trajectory;
 typedef std::vector<descartes_core::TrajectoryPtPtr> Trajectory;
-const int NUM_DENSE_POINTS = 1000;
-Trajectory createTestTrajectory();
-Trajectory TEST_TRAJECTORY = createTestTrajectory();
+const int NUM_DENSE_POINTS = 100;
+const int NUM_SPARSE_POINTS = 1000; 
+std::string dense = "dense";
+std::string sparse = "sparse";
+// Trajectory createTestTrajectory();
+// Trajectory TEST_TRAJECTORY_SPARSE = createTestTrajectory("sparse");
+// Trajectory TEST_TRAJECTORY_SPARSE = createTestTrajectory("dense");
 
-Trajectory createTestTrajectory()
-{
-  ROS_INFO_STREAM("Creating test trajectory with "<<NUM_DENSE_POINTS<<" points");
+Trajectory createTestTrajectory(std::string const& planner_type)
+{ 
+  if(planner_type == "dense")
+  { 
+    ROS_INFO_STREAM("Creating test trajectory with "<<NUM_DENSE_POINTS<<" points");
+  }
+  else
+  {
+    ROS_INFO_STREAM("Creating test trajectory with "<<NUM_SPARSE_POINTS<<" points");
+  }
+
   Trajectory traj;
   std::vector<std::tuple<double, double>>joint_bounds = {std::make_tuple(0,M_PI),
                                                          std::make_tuple(-M_PI_2,M_PI_2),
@@ -82,22 +98,48 @@ Trajectory createTestTrajectory()
   std::vector<double> deltas;
   for(auto& e:joint_bounds)
   {
-    double d = (std::get<1>(e)- std::get<0>(e))/NUM_DENSE_POINTS;
-    deltas.push_back(d);
+    if(planner_type == "dense")
+    {
+      double d = (std::get<1>(e)- std::get<0>(e))/NUM_DENSE_POINTS;
+      deltas.push_back(d);
+    }
+
+    if(planner_type == "sparse")
+    {
+      double d = (std::get<1>(e)- std::get<0>(e))/NUM_SPARSE_POINTS;
+      deltas.push_back(d);
+    }
   }
 
   // creating trajectory points
   std::vector<double> joint_vals(deltas.size(),0);
-  traj.reserve(NUM_DENSE_POINTS);
-  for(int i = 0 ; i < NUM_DENSE_POINTS; i++)
-  {
-    for(int j = 0; j < deltas.size(); j++)
+  if(planner_type =="dense")
+  {  
+    traj.reserve(NUM_DENSE_POINTS);
+    for(int i = 0 ; i < NUM_DENSE_POINTS; i++)
     {
-      joint_vals[j] = std::get<0>(joint_bounds[j]) + deltas[j]*i;
-    }
+      for(int j = 0; j < deltas.size(); j++)
+      {
+        joint_vals[j] = std::get<0>(joint_bounds[j]) + deltas[j]*i;
+      }
 
-    TrajectoryPtPtr tp(new TestPoint(joint_vals));
-    traj.push_back(tp);
+      TrajectoryPtPtr tp(new TestPoint(joint_vals));
+      traj.push_back(tp);
+    }
+  } 
+  if(planner_type =="sparse")
+  {  
+    traj.reserve(NUM_SPARSE_POINTS);
+    for(int i = 0 ; i < NUM_SPARSE_POINTS; i++)
+    {
+      for(int j = 0; j < deltas.size(); j++)
+      {
+        joint_vals[j] = std::get<0>(joint_bounds[j]) + deltas[j]*i;
+      }
+
+      TrajectoryPtPtr tp(new TestPoint(joint_vals));
+      traj.push_back(tp);
+    }
   }
   return traj;
 }
@@ -121,10 +163,18 @@ TYPED_TEST_P(DescartesPlannerTest, configure)
 
 TYPED_TEST_P(DescartesPlannerTest, planPath)
 {
-  ROS_INFO_STREAM("Testing planPath() with "<<NUM_DENSE_POINTS<<" points");
   RobotModelConstPtr robot(new ThreeDOFRobot());
   this->planner_->initialize(robot);
-  EXPECT_TRUE(this->planner_->planPath(TEST_TRAJECTORY));
+  if(dynamic_cast<SparsePlanner*>(this->planner_))
+  { 
+    ROS_INFO_STREAM("Testing planPath() with "<<NUM_SPARSE_POINTS<<" points");
+    EXPECT_TRUE(this->planner_->planPath(createTestTrajectory(sparse)));
+  }  
+  if(dynamic_cast<DensePlanner*>(this->planner_))
+  { 
+    ROS_INFO_STREAM("Testing planPath() with "<<NUM_DENSE_POINTS<<" points");
+    EXPECT_TRUE(this->planner_->planPath(createTestTrajectory(dense)));
+  }
 }
 
 TYPED_TEST_P(DescartesPlannerTest, getPath)
@@ -132,9 +182,18 @@ TYPED_TEST_P(DescartesPlannerTest, getPath)
   std::vector<descartes_core::TrajectoryPtPtr> path;
   RobotModelConstPtr robot(new ThreeDOFRobot());
   this->planner_->initialize(robot);
-  this->planner_->planPath(TEST_TRAJECTORY);
-  EXPECT_TRUE(this->planner_->getPath(path));
-  EXPECT_TRUE(path.size() == NUM_DENSE_POINTS);
+  if(dynamic_cast<SparsePlanner*>(this->planner_))
+  { 
+    EXPECT_TRUE(this->planner_->planPath(createTestTrajectory(sparse)));
+    EXPECT_TRUE(this->planner_->getPath(path));
+    EXPECT_TRUE(path.size() == NUM_SPARSE_POINTS); 
+  }  
+  if(dynamic_cast<DensePlanner*>(this->planner_))
+  { 
+    EXPECT_TRUE(this->planner_->planPath(createTestTrajectory(dense)));
+    EXPECT_TRUE(this->planner_->getPath(path));
+    EXPECT_TRUE(path.size() == NUM_DENSE_POINTS);
+  }
 }
 
 REGISTER_TYPED_TEST_CASE_P(DescartesPlannerTest, initialize, configure, planPath, getPath);
