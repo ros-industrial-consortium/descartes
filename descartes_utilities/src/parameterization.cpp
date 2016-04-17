76e8a30beea84d528c7e6387ff3fb2c069a1bea8
Jonathan Meyer <jmeyer@swri.org>
2016-04-16 20:37:42 -0500

    Points with equal time values will lead to NaNs. This commit adds a check for this scenario.

--- descartes_utilities/src/parameterization.cpp
+++ descartes_utilities/src/parameterization.cpp
@@ -53,9 +53,10 @@ bool descartes_utilities::toCubicSplines(const std::vector<trajectory_msgs::Join
   {
     // check time to ensure consistency
     const auto tm = traj[i].time_from_start.toSec();
-    if (tm < last_time_seen)
+    if (i > 0 && tm <= last_time_seen)
     {
-      logError("%s: 'time_from_start' fields must be sorted in increasing order", __FUNCTION__);
+      logError("%s: 'time_from_start' fields must be sorted in increasing order (index %lu = %f, index %lu = %f)", 
+        __FUNCTION__, static_cast<unsigned long>(i-1), last_time_seen, static_cast<unsigned long>(i), tm);
       return false;
     }
 
@@ -67,8 +68,9 @@ bool descartes_utilities::toCubicSplines(const std::vector<trajectory_msgs::Join
     // check to make sure nothing funky about the size of the positions
     if (traj[i].positions.size() != dof)
     {
-      logError("%s: 'Positions' field of trajectory point at index %u does not have the same size as first",
-        __FUNCTION__, static_cast<unsigned>(i));
+      logError("%s: 'Positions' field of trajectory point at index %lu does not have the same size as first (%lu vs %lu)",
+        __FUNCTION__, static_cast<unsigned long>(i), static_cast<unsigned long>(dof), 
+        static_cast<unsigned long>(traj[i].positions.size()));
       return false;
     }
 
@@ -97,12 +99,26 @@ bool descartes_utilities::setDerivatesFromSplines(std::vector<trajectory_msgs::J
     return false;
   }
 
+  return setDerivatesFromSplines(splines, traj);
+}
+
+bool descartes_utilities::setDerivatesFromSplines(const std::vector<SplineInterpolator>& splines,
+                                                  std::vector<trajectory_msgs::JointTrajectoryPoint>& traj)
+{
   const auto dof = splines.size();
 
   // Sample each parameter at the nominal time point
   for (auto& pt : traj)
   {
     auto tm = pt.time_from_start.toSec();
+
+    // sanity check the DOF of the splines with the DOF of the input path
+    if (pt.positions.size() != dof)
+    {
+      logError("%s: Splines vector is of size %lu and input trajectory point (time %f) has 'positions' field of size %lu",
+        __FUNCTION__, static_cast<unsigned long>(dof), tm, static_cast<unsigned long>(pt.positions.size()));
+      return false;
+    }
 
     // ensure there is room
     if (pt.velocities.size() != dof) pt.velocities.resize(dof);
@@ -116,5 +132,41 @@ bool descartes_utilities::setDerivatesFromSplines(std::vector<trajectory_msgs::J
     }
   }
 
+  return true;
+}
+
+bool descartes_utilities::resampleTrajectory(const std::vector<SplineInterpolator>& splines,
+                                             double start_tm, double end_tm, double tm_step,
+                                             std::vector<trajectory_msgs::JointTrajectoryPoint>& traj)
+{
+  if (start_tm >= end_tm)
+  {
+    logError("%s: 'start_tm' must be greater than or equal to 'end_tm' (%f vs %f)", __FUNCTION__,
+      start_tm, end_tm);
+    return false;
+  }
+
+  const auto dof = splines.size();
+  std::vector<trajectory_msgs::JointTrajectoryPoint> new_traj;
+
+  for (double tm = start_tm; tm < end_tm; tm += tm_step)
+  {
+    auto pt = trajectory_msgs::JointTrajectoryPoint();
+    pt.positions.resize(dof);
+    pt.velocities.resize(dof);
+    pt.accelerations.resize(dof);
+    pt.time_from_start = ros::Duration(tm);
+
+    for (auto i = 0; i < dof; i++)
+    {
+      pt.positions[i] = splines[i].position(tm);
+      pt.velocities[i] = splines[i].velocity(tm);
+      pt.accelerations[i] = splines[i].acceleration(tm);
+    }
+
+    new_traj.push_back(pt);
+  }
+
+  traj = new_traj;
   return true;
 }
