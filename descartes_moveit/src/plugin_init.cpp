1d3805c4efe3fdf25cf4cf26ce69fd8c6a615940
Achille <achille.verheye@gmail.com>
2019-07-06 16:49:16 -0700

    Create a moveit state adapter for the jaco3 custom IK solver. Copy pasta from IKFastMoveitStateAdapter

--- descartes_moveit/src/plugin_init.cpp
+++ descartes_moveit/src/plugin_init.cpp
@@ -5,6 +5,8 @@
 #include <pluginlib/class_list_macros.h>
 #include <descartes_moveit/moveit_state_adapter.h>
 #include <descartes_moveit/ikfast_moveit_state_adapter.h>
+#include <descartes_moveit/jaco3_moveit_state_adapter.h>
 
 PLUGINLIB_EXPORT_CLASS(descartes_moveit::MoveitStateAdapter, descartes_core::RobotModel)
 PLUGINLIB_EXPORT_CLASS(descartes_moveit::IkFastMoveitStateAdapter, descartes_core::RobotModel)
+PLUGINLIB_EXPORT_CLASS(descartes_moveit::Jaco3MoveitStateAdapter, descartes_core::RobotModel)
